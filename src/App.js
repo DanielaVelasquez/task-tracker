44d7adb4e74a026f24699a52f1be4e2c9f33e0e8
danielavelasquez <dvelasquez@prontoforms.com>
2021-06-23 22:08:17 -0400

    Global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Don't forget to be awesome",
+			day: 'Jan 26 at 11:00 am',
+			reminder: true
+		},
+		{
+			id: 2,
+			text: "Don't forget to enjoy",
+			day: 'Dec 4 at 02:00 am',
+			reminder: true
+		}
+	])
+
   return (
     <div className="container">
       <Header/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
