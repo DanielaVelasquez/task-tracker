c368737d1c9493bdec05663634283fe5649c98d2
danielavelasquez <dvelasquez@prontoforms.com>
2021-06-25 17:04:48 -0400

    Adding tasks to the reminder

--- src/App.js
+++ src/App.js
@@ -33,13 +33,21 @@ function App() {
     )
   }
 
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random()  * 1000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {
         tasks.length > 0 ?
-        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):
+        (<Tasks tasks={tasks} 
+            onDelete={deleteTask} 
+            onToggle={toggleReminder}/>):
         ('No tasks')
       }
     </div>
