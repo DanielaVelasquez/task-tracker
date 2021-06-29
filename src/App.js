b42ab064fb30abe87019dccea3d7f2316f226841
danielavelasquez <dvelasquez@prontoforms.com>
2021-06-29 17:14:22 -0400

    Tasks are fetched from the server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([])
+
+  useEffect(()=>{
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+    return data
+  }
 
   const deleteTask = (id) =>{
     setTasks(tasks.filter((task)=> task.id !== id))
